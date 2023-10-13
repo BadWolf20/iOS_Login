1c672fc7a41c31b35ed276df301a752b391f0302
BadWolf20 <Roman.Pichugin20@gmail.com>
2023-10-13 15:49:48 +0700

    Extension for UIview

--- iOS_Login/Extensions.swift
+++ iOS_Login/Extensions.swift
@@ -58,3 +58,27 @@ extension UIViewController {
     }
 }
 
+extension UIView {
+    func safeResource<T>(_ resource: T?) -> T where T: AnyObject {
+        switch T.self {
+        case is UIImage.Type:
+            if let image = resource {
+                return image
+            } else {
+                print("Ошибка: не удалось загрузить изображение. Возвращение дефолтного изображения.")
+                return UIImage(named: "DefaultImage") as! T
+            }
+
+        case is UIColor.Type:
+            if let color = resource {
+                return color
+            } else {
+                print("Ошибка: не удалось загрузить цвет. Возвращение дефолтного цвета.")
+                return UIColor(named: "DefaultColor") as! T
+            }
+
+        default:
+            fatalError("Тип \(T.self) не поддерживается.")
+        }
+    }
+}
